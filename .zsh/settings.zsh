bff835056761e425c8e06e027cb46df983111abe
Daniel Voogsgerd <daniel@voogsgerd.nl>
2018-12-09 18:36:04 +0100

    Move settings to X11/Wayland session

--- .zsh/settings.zsh
+++ .zsh/settings.zsh
@@ -17,19 +17,3 @@ setopt MULTIOS
 setopt GLOB_COMPLETE
 setopt PUSHD_MINUS
 
-
-# Defaults
-export EDITOR="nano"
-export GIT_EDITOR="nano"
-export VISUAL="gedit"
-
-export LANG="en_US.UTF-8"
-export EDITOR VISUAL GIT_EDITOR
-
-# XDG Settings
-export XDG_CONFIG_HOME="$HOME/.config"
-
-# Easy cding in projects
-export CDPATH="$HOME/Projects"
-
-export LESS="FRX"
