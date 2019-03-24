e9743f7f67077c9e3b4848bf5f26b9665d0c04ac
Daniel Voogsgerd <daniel@voogsgerd.nl>
2019-03-24 12:33:05 +0100

    New Prompt

--- .config/zsh/conf/prompt.zsh
+++ .config/zsh/conf/prompt.zsh
@@ -1,8 +1,11 @@
 setopt PROMPT_SUBST
 source $ZDOTDIR/plugins/vcs/zshrc.sh
 
-# Besides an ordinairy prompt I also set the terminal window to contain the executed command. Thanks to Brian from bstpierre
+# ┌───────┐
+# │ Title │
+# └───────┘
 function title() {
+	# Credits to Brian St. Pierre 
     # escape '%' chars in $1, make nonprintables visible
     local a=${(V)1//\%/\%\%}
 
@@ -20,15 +23,16 @@ function title() {
     esac
 }
 
-# precmd is called just before the prompt is printed
+# ┌──────────────────────┐
+# │ Before Prompt is ran │
+# └──────────────────────┘
 function precmd() {
     title "zsh" "%m:%55<...<%~"
-	if [ -f $HISTFILE ]; then
-		commandcount=$(wc -l < $HISTFILE)
-	fi
 }
 
-# preexec is called just before any command line is executed
+# ┌────────────────────────────────┐
+# │ Before the command is executed │
+# └────────────────────────────────┘
 function preexec() {
     title "$1" "%m:%35<...<%~"
 }
@@ -36,50 +40,49 @@ function preexec() {
 function shell_icon {
 	if [[ "$(stat -f -c %T .)" == 'cifs' ]]; then
 		echo "☁"
-	elif [[ "$USER" == "root" ]]; then
-		echo "#"
 	else
-		echo "$"
+		echo "%(?..%F{001})%(!.#.$)%f"
 	fi
 }
 
-function prompt() {
-	local p_hostname="%F{005}"
-	local p_path="%F{027}"
+# ┌─────────────┐
+# │ Left Prompt │
+# └─────────────┘
+PROMPT=""
 
-	PROMPT=""
+# Username if not my own user
+if [[ "$USER" != "$username" ]]; then
+	PROMPT+="%(!.%F{001}.%F{011})$USER"
+fi
 
-	if [[ "$USER" == "root" ]]; then
-		PROMPT+="%F{001} $USER"
-	elif [[ "$USER" != "$username" ]]; then
-		PROMPT+="%F{011} $USER"
-	fi
+# Hostname if connected via SSH
+if [ -n "$SSH_CONNECTION" ]; then
+	PROMPT+="%m"
+fi
 
-	PROMPT+=" $p_path"
-	PROMPT+="%~ "
-	PROMPT+="%f"
-	PROMPT+='$(vcs_super_info_wrapper)'
-	PROMPT+='$(shell_icon)'
-	PROMPT+="%f "
+# Path
+PROMPT+=" %F{027}"
+PROMPT+="%~ "
 
-	RPROMPT=""
+PROMPT+="%f"
 
-	if [ -z "$commandcount" ]; then
-		RPROMPT+="%F{004}["
-		RPROMPT+="%F{012}"'${commandcount}'
-		RPROMPT+="%F{004}]"
-	fi
+# Shell icon
+PROMPT+='$(shell_icon)'
 
-	RPROMPT+="%F{004}"
-	RPROMPT+="[%(?,%F{012},%F{001})"
-	RPROMPT+="%?"
-	RPROMPT+="%F{004}]"
+# Set color to normal and add a space for seperation
+PROMPT+="%f "
 
-	RPROMPT+="%F{004}["
-	RPROMPT+="%F{012}%*"
-	RPROMPT+="%F{004}]"
-	RPROMPT+="%f"
-}
+# ┌──────────────┐
+# │ Right Prompt │
+# └──────────────┘
+RPROMPT=""
 
+RPROMPT+="%F{004}"
 
-prompt
+# Return code
+RPROMPT+="%(?..%F{001}[%?] )"
+
+# VCS info
+RPROMPT+='$(vcs_super_info)'
+
+RPROMPT+="%f"
